e26ebd3b8a4dc5af16e1e379ff9c52306e7ff963
Byteme-dot <124488563+Byteme-dot@users.noreply.github.com>
2026-09-08 19:24:00 +0530

    Setup Rust Cargo Project

---
+++ engine/target/debug/deps/engine-1fa8ea3ea208a80c.d
@@ -0,0 +1,5 @@
+C:\Users\abhi_devnull\Documents\GitHub\Aperion\engine\target\debug\deps\engine-1fa8ea3ea208a80c.d: src\main.rs
+
+C:\Users\abhi_devnull\Documents\GitHub\Aperion\engine\target\debug\deps\libengine-1fa8ea3ea208a80c.rmeta: src\main.rs
+
+src\main.rs:
---
+++ engine/target/debug/deps/engine.d
@@ -0,0 +1,5 @@
+C:\Users\abhi_devnull\Documents\GitHub\Aperion\engine\target\debug\deps\engine.d: src\main.rs
+
+C:\Users\abhi_devnull\Documents\GitHub\Aperion\engine\target\debug\deps\engine.exe: src\main.rs
+
+src\main.rs:
